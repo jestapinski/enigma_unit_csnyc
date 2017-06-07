23bee03dbe95e97224f7e297d0bfb3206b52be3d
Jordan Stapinski <jordanstapinski@MacBook-Pro-15.local>
2017-06-07 10:09:49 -0400

    Added starting splash screen with Materialize CSS and JS framework installed

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+    app = express(),
+    engines = require('consolidate'),
+    MongoClient = require('mongodb').MongoClient,
+    bodyParser = require('body-parser'),
+    assert = require('assert');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+MongoClient.connect('mongodb://localhost:27017/movie_information', function(err, db) {
+
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
+
+	app.get("/", function(req, res, next) {
+		res.render('splash', {'unit_name': 'Enigma: An Introduction to Cryptography'});
+	});
+
+	// Running the web server
+	var server = app.listen(3000, function() {
+	    var port = server.address().port;
+	    console.log('Express server listening on port %s.', port);
+	});
+}); 
