1264740b4c1465cfb3778a019a8914b3bd758f12
Jordan Stapinski <jestapinski@yahoo.com>
2017-06-12 09:48:57 -0400

    Removed commented JS code

--- public/js/encrypt.js
+++ public/js/encrypt.js
@@ -22,13 +22,9 @@ function drawWheel(){
 
     ctx.clearRect(0, 0, 500, 200)
 
-    // ctx.strokeStyle = "black";
-    // ctx.lineWidth = 2;
-
     var colors = ["#F5E5C0", "#FFFFFF", "#F5E5C0", "#F5E5C0"]
 
     for (var i = 0; i < numSections; i++) {
-      // ctx.clearRect(0, i * 25, 25, 25)
       ctx = canvas.getContext("2d")
       console.log(colors)
       console.log(i)
@@ -64,13 +60,9 @@ function redraw(step, direction){
     ctx = canvas.getContext("2d")
       for (var i = 0; i < numSections + 1; i++) {
         ctx = canvas.getContext("2d")
-        // console.log(colors)
-        // console.log(i)
         if (i < colors.length){
-          // console.log(colors[i])
           ctx.fillStyle = colors[i];
 
-          // console.log(ctx.fillStyle)
           ctx.fillRect(0, i * boxHeight, boxWidth, boxHeight);
         }
         ctx.fillStyle = "#000000";
@@ -86,8 +78,6 @@ function redraw(step, direction){
           ctx.fillText(numbers[numStart].toString(), 25, 30 + i * 50 - (step))
         } else {
           console.log("step")
-          // console.log(step)
-          // console.log(30 + i * 50 + (step))
           ctx.fillText(numbers[(startIndex + i) % numbers.length].toString(), 25, -20 + i * 50 + step )
         }
 
@@ -111,18 +101,11 @@ function spinWheelUp(){
     var numSections = 4
     ctx = canvas.getContext("2d")
 
-    // ctx.clearRect(0, 0, 500, 200)
-
     var colors = ["#F5E5C0", "#FFFFFF", "#F5E5C0", "#F5E5C0"]
     for (var step = 0; step < boxHeight; step++){
-      // ctx.clearRect(0, 0, 500, 200)
-      //redraw
-      // redraw(step);
       setTimeout(redraw, 100, step, "up");
       //setTimeout and pass step
-
     }
-
     startIndex++;
     startIndex %= numbers.length;
     console.log("new start index");
@@ -138,32 +121,30 @@ function spinWheelDown(){
     var numSections = 4
     ctx = canvas.getContext("2d")
 
-    // ctx.clearRect(0, 0, 500, 200)
-
     var colors = ["#F5E5C0", "#FFFFFF", "#F5E5C0", "#F5E5C0"]
     for (var step = 0; step < boxHeight; step++){
-      // ctx.clearRect(0, 0, 500, 200)
-      //redraw
-      // redraw(step);
       setTimeout(redraw, 100, step, "down");
       //setTimeout and pass step
-
     }
     //Move for correctness
     startIndex--;
     if (startIndex == -1){
       startIndex = numbers.length - 1;
     }
-    // startIndex %= numbers.length;
-    console.log("new start index");
-    console.log(startIndex);
-    // drawWheel()
   }
+}
+
+function plaintext_not_valid(text){
+  return (text == "");
 }
 
 function runEncryption(){
   remove_tooltip();
   var passed_text_value = plaintext_value.value;
+  if (plaintext_not_valid(passed_text_value)){
+    alert("Plaintext should not be blank!");
+    return;
+  }
   // TODO rewrite below more cleanly
   var selected_value = (startIndex + 1) % numbers.length;
   var shifting_value = numbers[selected_value];//.toString();
@@ -177,71 +158,64 @@ function runEncryption(){
   canvas.height = 200;
   canvas.id = "black_box_canvas";
 
-  // if (black_box_text.style.display === 'none'){
-  //   black_box_text.style.display = 'block';
-  // } else {
-    // var parent = document.getElementById('middle-box');
-    // console.log(parent);
-    console.log(black_box_text.parentNode);
-    parentNode.appendChild(canvas);
-    black_box_text.parentNode.removeChild(black_box_text);
-    button_parent_node.removeChild(encrypt);
-    // return;
-    // black_box_text.style.display = 'none';
-    if (canvas.getContext){
-      var boxWidth = 50
-      var boxHeight = 50
-      var numSections = 4
-      ctx = canvas.getContext("2d")
-      var offsets = [0, 100];
+  console.log(black_box_text.parentNode);
+  parentNode.appendChild(canvas);
+  black_box_text.parentNode.removeChild(black_box_text);
+  button_parent_node.removeChild(encrypt);
 
-      ctx.clearRect(0, 0, 500, 200)
+  if (canvas.getContext){
+    var boxWidth = 50
+    var boxHeight = 50
+    var numSections = 4
+    ctx = canvas.getContext("2d")
+    var offsets = [0, 100];
 
-      // ctx.strokeStyle = "black";
-      // ctx.lineWidth = 2;
+    ctx.clearRect(0, 0, 500, 200)
 
-      var colors = ["#F5E5C0", "#FFFFFF", "#F5E5C0", "#F5E5C0"]
-      for (var offset = 0; offset < offsets.length; offset++){
-        for (var i = 0; i < passed_text_value.length; i++) {
-          // ctx.clearRect(0, i * 25, 25, 25)
-          ctx = canvas.getContext("2d")
-          console.log(colors)
-          console.log(i)
-          console.log(colors[i])
-          ctx.fillStyle = "#FFFFFF";
+    // ctx.strokeStyle = "black";
+    // ctx.lineWidth = 2;
 
-          console.log(ctx.fillStyle)
-          ctx.fillRect(i * boxWidth, offsets[offset], boxWidth, boxHeight);
-          ctx.fillStyle = "#000000";
-          ctx.font = "30px Arial";
-          ctx.textAlign = "center";
-          // TODO fix below value
-          if (offset == 0){
-            ctx.fillText(passed_text_value[i], 25 + i * 50, offsets[offset] + 30)            
-          } else {
-            //Write a method for encryption
-            ctx.fillText(String.fromCharCode(passed_text_value[i].charCodeAt(0) + shifting_value), 25 + i * 50, offsets[offset] + 30)
-          }
+    var colors = ["#F5E5C0", "#FFFFFF", "#F5E5C0", "#F5E5C0"]
+    for (var offset = 0; offset < offsets.length; offset++){
+      for (var i = 0; i < passed_text_value.length; i++) {
+        // ctx.clearRect(0, i * 25, 25, 25)
+        ctx = canvas.getContext("2d")
+        console.log(colors)
+        console.log(i)
+        console.log(colors[i])
+        ctx.fillStyle = "#FFFFFF";
 
-          ctx.beginPath();
-          ctx.moveTo((i) * boxWidth, offsets[offset]);
-          ctx.lineTo((i) * boxWidth, offsets[offset] + boxHeight);
-          ctx.stroke();
-
-          ctx.beginPath();
-          ctx.moveTo((i + 1) * boxWidth, offsets[offset]);
-          ctx.lineTo((i + 1) * boxWidth, offsets[offset] + boxHeight);
-          ctx.stroke();
+        console.log(ctx.fillStyle)
+        ctx.fillRect(i * boxWidth, offsets[offset], boxWidth, boxHeight);
+        ctx.fillStyle = "#000000";
+        ctx.font = "30px Arial";
+        ctx.textAlign = "center";
+        // TODO fix below value
+        if (offset == 0){
+          ctx.fillText(passed_text_value[i], 25 + i * 50, offsets[offset] + 30)            
+        } else {
+          //Write a method for encryption
+          ctx.fillText(String.fromCharCode(passed_text_value[i].charCodeAt(0) + shifting_value), 25 + i * 50, offsets[offset] + 30)
         }
+
+        ctx.beginPath();
+        ctx.moveTo((i) * boxWidth, offsets[offset]);
+        ctx.lineTo((i) * boxWidth, offsets[offset] + boxHeight);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo((i + 1) * boxWidth, offsets[offset]);
+        ctx.lineTo((i + 1) * boxWidth, offsets[offset] + boxHeight);
+        ctx.stroke();
       }
     }
-    var stringArray = passed_text_value.split("");
-    var mappedArray = stringArray.map(function(c){
-      return String.fromCharCode(c.charCodeAt(0) + shifting_value);
-    })
-    var newText = mappedArray.join("");
-    output_text.value = newText;    
-  // }
+  }
+  var stringArray = passed_text_value.split("");
+  var mappedArray = stringArray.map(function(c){
+    return String.fromCharCode(c.charCodeAt(0) + shifting_value);
+  })
+  var newText = mappedArray.join("");
+  output_text.value = newText;    
 }
 
 drawWheel()
