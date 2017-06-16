fed6ab8feedf3ab3e2c06f3d6b7b795edfbcc1e7
Jordan Stapinski <jstapins@andrew.cmu.edu>
2017-06-16 16:20:07 -0400

    Added skeleton code for implementing the wheel for shift index

--- public/password/password-encrypt.js
+++ public/password/password-encrypt.js
@@ -1,14 +1,77 @@
 // Jordan Stapinski
 // Enigma Password Encryption JS file
 
+var word_index_wheel_canvas = document.getElementById('word_index_wheel');
+var word_shift_canvas = document.getElementById('word_shift_canvas');
+var current_word_index = 0;
+var given_word = '';
+
 
 function run_encryption(){
 	$('#word_shift_text').removeAttr('hidden');
 }
 
+/*
+	run_start_modal
+
+	No input values
+	No return value
+
+	Opens the starting modal (wrapped in a function for cleanliness)
+*/
 function run_start_modal(){
 	$('#modal_encrypt').modal('open');
 }
 
-encrypt.addEventListener("click", run_encryption);
+/*
+	draw_outer_wheel
+
+	ctx: The drawing context for the word_index_wheel_canvas
+	No return value
+
+	Draws the outer, dark wheel for the word index wheel
+*/
+function draw_outer_wheel(ctx){
+	return;
+}
+
+/*
+	draw_inner_triangle
+
+	ctx: The drawing context for the word_index_wheel_canvas
+	No return value
+
+	Draws the inner, light section for the selected word index	
+*/
+function draw_inner_triangle(ctx){
+	return;
+}
+
+/*
+	draw_current_index_value
+
+	ctx: The drawing context for the word_index_wheel_canvas
+	angle: The angle at which we want to draw the value (spinning around the wheel)
+	No return value
+
+	Draws the current word index value, along with its letter, on the wheel
+*/
+function draw_current_index_value(ctx, angle){
+	if (!angle){
+		angle = 0;
+	}
+	return;
+}
+
+function draw_index_wheel(){
+	var ctx;
+	if (word_index_wheel_canvas.getContext){
+		ctx = word_index_wheel_canvas.getContext('2d');
+		draw_outer_wheel(ctx);
+		draw_inner_triangle(ctx);
+		draw_current_index_value(ctx);
+	}
+}
+
+encrypt.addEventListener('click', run_encryption);
 setTimeout(run_start_modal, 600);
